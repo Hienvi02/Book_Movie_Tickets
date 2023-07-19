6a8b6fe73d88d74a163b9b342dff0d7a6c0df229
hienvi <hienvi16012002@gmail.com>
2023-07-19 14:06:56 +0700

    create process

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { MegaMenuModule } from 'primeng/megamenu';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ButtonModule,
+    MegaMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
